64dd45170f62257c544a24d574f87e1155b30299
Federico Iñigo <inigofede@gmail.com>
2025-11-19 16:53:19 -0300

    fix7button

---
+++ scripts/pd-test.ts
@@ -0,0 +1,12 @@
+import { searchDealsByMapacheAssigned } from "@/lib/pipedrive";
+
+async function main() {
+  const name = process.argv[2] ?? "Federico Iñigo";
+  const deals = await searchDealsByMapacheAssigned(name);
+  console.log(JSON.stringify(deals, null, 2));
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
